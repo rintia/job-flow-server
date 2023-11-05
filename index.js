37424e632014fb26a205a3e3c710c207592a4377
Rintia Islam <rintiaislam110@gmail.com>
2023-11-05 15:22:21 +0600

    initial server setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('job flow is running')
+})
+
+app.listen(port, () => {
+    console.log(`Job Flow Server is running on port ${port}`)
+})
