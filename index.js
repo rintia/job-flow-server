c2843e7ad60e9c9f8d3d3105cf4ed4ed9f22fefa
Rintia Islam <rintiaislam110@gmail.com>
2023-11-05 22:30:07 +0600

    Mongodb database created for jobs

--- index.js
+++ index.js
@@ -9,6 +9,46 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+console.log(process.env.DB_PASS)
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cj9n1qe.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const jobsCollection = client.db('jobFlowDB').collection('jobs');
+
+    app.get('/jobs', async (req, res) => {
+        const cursor = jobsCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 
 
